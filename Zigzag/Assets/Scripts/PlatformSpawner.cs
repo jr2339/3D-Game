6e9ccd81de7711b28a80a823371584dc82bb10ef
jr2339 <jr2339@nau.edu>
2016-12-01 02:09:52 -0700

    Figure Out Spawn function

--- Zigzag/Assets/Scripts/PlatformSpawner.cs
+++ Zigzag/Assets/Scripts/PlatformSpawner.cs
@@ -3,13 +3,37 @@ using System.Collections;
 
 public class PlatformSpawner : MonoBehaviour {
 
+	public GameObject platform;
+	Vector3 lastPos;
+	float size;
+
 	// Use this for initialization
 	void Start () {
-	
+		lastPos = platform.transform.position;
+		size = platform.transform.localScale.x;
+
+		for(int i=0; i<5;i++){
+			SpawnZ ();
+		}
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	void SpawnX(){
+
+		Vector3 pos = lastPos;
+		pos.x += size;
+		lastPos = pos;
+		Instantiate (platform, pos, Quaternion.identity);
+	}
+
+	void SpawnZ(){
+		Vector3 pos = lastPos;
+		pos.z += size;
+		lastPos = pos;
+		Instantiate (platform, pos, Quaternion.identity);
 	}
 }
