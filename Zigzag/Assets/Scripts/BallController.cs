28ba845898d59270a220c23212b27c3b50eb5990
jr2339 <jr2339@nau.edu>
2016-12-01 01:55:39 -0700

    Still Need to figure out

--- Zigzag/Assets/Scripts/BallController.cs
+++ Zigzag/Assets/Scripts/BallController.cs
@@ -6,47 +6,45 @@ public class BallController : MonoBehaviour {
 	private float speed;
 	bool started;
 	bool gameOver;
+
 	Rigidbody rb;
 
 	void Awake(){
 		rb = GetComponent<Rigidbody> ();
-
 	}
 	// Use this for initialization
 	void Start () {
 		started = false;
 		gameOver = false;
 	}
-
+	
 	// Update is called once per frame
 	void Update () {
-
 		if(!started){
 			if(Input.GetMouseButtonDown(0)){
 				rb.velocity = new Vector3 (speed, 0, 0);
 				started = true;
 			}
 		}
-		Debug.DrawRay (transform.position, Vector3.down, Color.red);
+		Debug.DrawRay (transform.position,Vector3.down,Color.red);
 
 		if (!Physics.Raycast (transform.position, Vector3.down, 1f)) {
 			gameOver = true;
-			rb.velocity = new Vector3 (0, -25f, 0);
-			Camera.main.GetComponent<CameraFollow> ().gameOver = true;
+			rb.velocity = new Vector3 (0,-25f, 0);//fall down
+			Camera.main.GetComponent<CameraFollow>().gameOver = true;
 		}
 
-		if (Input.GetMouseButtonDown (0) && !gameOver) {
+		if(Input.GetMouseButtonDown(0) && !gameOver){
 			SwitchDirection ();
 		}
 	}
 
 	void SwitchDirection(){
-		if (rb.velocity.z > 0) {
+		if(rb.velocity.z > 0){
 			rb.velocity = new Vector3 (speed, 0, 0);
-
 		}
 		else if(rb.velocity.x > 0){
-			rb.velocity = new Vector3 (0,0,speed);
+			rb.velocity = new Vector3 (0, 0, speed);
 		}
 	}
 }
